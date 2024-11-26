ac52fb6d338fc23bc1d3bc44f49b3f55516ad79b
Diego_Acosta <dacogon1003@g.educaand.es>
2024-11-26 13:11:29 +0100

    Editar numerosAleatorios

--- src/NumAleatorios/numerosAleatorios.java
+++ src/NumAleatorios/numerosAleatorios.java
@@ -7,7 +7,7 @@ public class numerosAleatorios {
 	public static void main(String[] args) {
 
 		Random random = new Random();
-		  for (int i = 0; i < 20; i++) {
+		  for (int i = 0; i < 30; i++) {
 			  int numeroAleatorio = random.nextInt(10) + 1;
 		      System.out.println(numeroAleatorio);
 		      }
